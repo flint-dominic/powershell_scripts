69a7ba0cdfe5e68bf75d245a73714a39d4a68940
Flint Domninic <31749352+flint-dominic@users.noreply.github.com>
2017-10-03 07:42:20 -0700

    cleared variable locations

--- set.bmc.addresses.ps1
+++ set.bmc.addresses.ps1
@@ -2,8 +2,8 @@ Param
 (
     [Parameter(Mandatory=$false)][string]$BMCDataFile,
     [Parameter(Mandatory=$false)][string]$BMCIPAddress,
-    [Parameter(Mandatory=$false)][string]$Password = "p@ssw0rd",
-    [Parameter(Mandatory=$false)][string]$UserName = "Administrator",
+    [Parameter(Mandatory=$false)][string]$Password = "<password>",
+    [Parameter(Mandatory=$false)][string]$UserName = "<username>",
     [Parameter(Mandatory=$false)][string]$NetId = "1",
     [Parameter(Mandatory=$false)][string]$NewIPAddress,
     [Parameter(Mandatory=$false)][string]$NetMask,
@@ -15,28 +15,28 @@ If (-not(Test-Path $IPMITool)) { Throw "ipmitool.exe could not be found in the c
 
 If ($BMCDataFile)
 {
-    $BMCData = Import-Csv $BMCDataFile
+    $BMCData = Import-CSV $BMCDataFile
     ForEach ($BMCNode In $BMCData)
     {
         If ($BMCNode.Skip -eq "FALSE")
         {
-            If ($BMCNode.Name) { Write-Output ( "NAME: " + $BMCNode.Name) }
+            If ($BMCNode.Name) { Write-Output ( "Name: " + $BMCNode.Name) }
             If ($NetMask -or $Gateway)
             {
                 If ($Gateway)
                 {
-                    Write-Output ("    SET-GATEWAY  NEW_GW: " + $Gateway)
+                    Write-Output ("    Set-Gateway  NEW_GW: " + $Gateway)
                     & $IPMITool -I lanplus -H $BMCNode.NewIP -P $Password -U $UserName lan set $NetId defgw ipaddr $Gateway
                 }
                 If ($NetMask)
                 {
-                    Write-Output ("    SET-NETMASK  NEW_MASK: " + $NetMask)
+                    Write-Output ("    Set-Netmask  NEW_MASK: " + $NetMask)
                     & $IPMITool -I lanplus -H $BMCNode.NewIP -P $Password -U $UserName lan set $NetId netmask $NetMask
                 }
             }
             If ($BMCNode.NewIP)
             {
-                Write-Output ("    SET-IP  OLD_IP: " + $BMCNode.BMCIP + "  NEW_IP: " + $BMCNode.NewIP)
+                Write-Output ("    Set-IP  OLD_IP: " + $BMCNode.BMCIP + "  NEW_IP: " + $BMCNode.NewIP)
                 & $IPMITool -I lanplus -H $BMCNode.BMCIP -P $Password -U $UserName lan set $NetId ipaddr $BMCNode.NewIP
             }
         }
@@ -49,12 +49,12 @@ ElseIf ($BMCIPAddress)
     {
         If ($Gateway)
         {
-            Write-Output ("    SET-GATEWAY  NEW_GW: " + $Gateway)
+            Write-Output ("    Set-Gateway  NEW_GW: " + $Gateway)
             & $IPMITool -I lanplus -H $BMCIPAddress -P $Password -U $UserName lan set $NetId defgw ipaddr $Gateway
         }
         If ($NetMask)
         {
-            Write-Output ("    SET-NETMASK  NEW_MASK: " + $NetMask)
+            Write-Output ("    Set-Netmask  NEW_MASK: " + $NetMask)
             & $IPMITool -I lanplus -H $BMCIPAddress -P $Password -U $UserName lan set $NetId netmask $NetMask
         }
     }
