4a7fe7b84e546daedbe39f754acafba7778c8fdb
Flint Dominic <31749352+flint-dominic@users.noreply.github.com>
2017-11-29 14:36:16 -0800

    Update ad.to.csv.ps1
    added param, fixed variables

--- ad.to.csv.ps1
+++ ad.to.csv.ps1
@@ -1,6 +1,14 @@
-$users = Get-ADUser -Filter {Company -eq "<MyCompany>"} -Properties sn, givenName, displayName, description, Title | where {$_.Enabled -eq $true } | select Name, sn, givenName, displayName, description, Title
-$dir1 = Get-ChildItem -Path "<\\server\directory\Records\forms\dir1>" | Select-Object name
-# $dir2 = Get-ChildItem -Path "<\\server\directory\Records\ticket\dir2>" | select-object name
+Param
+(
+    [string]$MyCompany='CompanyName',
+    [string]$path='\\server\directory\Records\forms\dir1',
+	[string]$path2='\\server\directory\Records\forms\dir2',
+    [string]$outcsv='.\example.csv'
+)
+
+$users = Get-ADUser -Filter {Company -eq "$MyCompany"} -Properties sn, givenName, displayName, description, Title | where {$_.Enabled -eq $true } | select Name, sn, givenName, displayName, description, Title
+$dir1 = Get-ChildItem -Path "$path" | Select-Object name
+# $dir2 = Get-ChildItem -Path "$path2" | select-object name
 $outFile = @($users,$dir1)
 
 foreach($file in $dir1)
@@ -13,4 +21,4 @@ foreach($file in $dir1)
         $output += ($users,$file)
         }
     }
-$outFile | ConvertTo-CSV | Out-File .\example.csv
+$outFile | ConvertTo-CSV | Out-File $outcsv
