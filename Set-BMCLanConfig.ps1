2316bd50e8f2582d460b8e2702993923239258ec
Flint Dominic <31749352+flint-dominic@users.noreply.github.com>
2017-11-29 16:06:13 -0800

    Update Set-BMCLanConfig.ps1
    updated docs

--- Set-BMCLanConfig.ps1
+++ Set-BMCLanConfig.ps1
@@ -1,3 +1,31 @@
+<#
+.SYNOPSIS
+Change BMC IP's.
+
+.DESCRIPTION
+This script will udpate BMC IP's using a CSV file using input creds
+
+.PARAMETER ipmitoolPath
+location of the ipmi tool path
+
+.PARAMETER CSVPath
+location of the BMC nodes to change
+
+.PARAMETER GatewayIP
+gateway address to change too
+
+.PARAMETER SubNetMask
+subnet mask to change too
+
+.PARAMETER $creds
+this will be prompted for
+
+.EXAMPLE
+.\Set-BMCLanConfig.ps1 -ipmitoolPath 'C:\tmp\' -CSVPath 'C:\tmp\bmclist.csv' -GatewayIP '192.168.0.1' -SubNetMask '255.255.255.0'
+This would use the stated variables to change the BMC addresses.
+
+#>
+
 param
 (
     [Parameter(Mandatory=$True)]
